fbf6c17f17cbfdfb067406a3b3fc4f2edced75f0
mjrchy <m.jrchyth@gmail.com>
2025-04-01 09:14:20 +0700

    Update upload.js

--- routes/upload.js
+++ routes/upload.js
@@ -5,7 +5,7 @@ const path = require("path");
 const fs = require("fs");
 
 // Ensure upload directory exists
-const uploadDir = path.join(__dirname, "uploads"); // This is mapped to a VM folder using Docker
+const uploadDir = "./uploads"; // This is mapped to a VM folder using Docker
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
 }
@@ -30,7 +30,7 @@ router.post("/", upload.single("image"), (req, res) => {
 
 // Serve Uploaded Images
 router.get("/:filename", (req, res) => {
-  const filePath = path.join(uploadDir, req.params.filename);
+  const filePath = path.join(__dirname, "..", uploadDir, req.params.filename);
   if (fs.existsSync(filePath)) {
     res.sendFile(filePath);
   } else {
