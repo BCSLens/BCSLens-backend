18a6508b0b4499d98b0281a8718c9698a93134dc
Sansan111 <shannavathon@gmail.com>
2025-11-26 00:23:15 +0700

    update

---
+++ .eslintrc.js
@@ -0,0 +1,67 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+    jest: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:security/recommended',
+    'plugin:node/recommended',
+    'standard'
+  ],
+  plugins: [
+    'security',
+    'node',
+    'import',
+    'promise'
+  ],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module'
+  },
+  rules: {
+    // Security-specific rules
+    'security/detect-object-injection': 'warn',
+    'security/detect-non-literal-fs-filename': 'warn',
+    'security/detect-eval-with-expression': 'error',
+    'security/detect-no-csrf-before-method-override': 'error',
+    'security/detect-unsafe-regex': 'error',
+    'security/detect-buffer-noassert': 'error',
+    'security/detect-child-process': 'warn',
+    'security/detect-disable-mustache-escape': 'error',
+    'security/detect-new-buffer': 'error',
+    'security/detect-non-literal-regexp': 'warn',
+    'security/detect-non-literal-require': 'warn',
+    'security/detect-possible-timing-attacks': 'warn',
+    'security/detect-pseudoRandomBytes': 'error',
+    
+    // Node.js specific
+    'node/no-unpublished-require': 'off', // Allow dev dependencies in tests
+    'node/no-missing-require': 'error',
+    'node/no-extraneous-require': 'error',
+    
+    // Code quality
+    'no-console': 'warn',
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'no-undef': 'error',
+    
+    // Import/Export
+    'import/no-unresolved': 'off', // Can be enabled if using module resolution
+    'import/order': ['warn', {
+      'groups': ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+      'newlines-between': 'always',
+      'alphabetize': { 'order': 'asc', 'caseInsensitive': true }
+    }]
+  },
+  ignorePatterns: [
+    'node_modules/',
+    'logs/',
+    'uploads/',
+    'venv/',
+    '*.log',
+    'coverage/',
+    '.env'
+  ]
+};
+
